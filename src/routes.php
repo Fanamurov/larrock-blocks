db20d08e9d639dcc70f90d07d8e955791f5f06f5
Alexandr Fanamurov <fanamura@gmail.com>
2017-07-06 13:30:58 +1000

    Fix

--- src/routes.php
+++ src/routes.php
@@ -2,6 +2,6 @@
 
 use Larrock\ComponentBlocks\AdminBlocksController;
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
     Route::resource('blocks', AdminBlocksController::class);
 });
