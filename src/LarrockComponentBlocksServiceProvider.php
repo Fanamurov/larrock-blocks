03f16a262731987e888f14a8976a5ed1e2098d0f
Alexandr Fanamurov <fanamurov@ya.ru>
2018-03-16 16:25:28 +1000

    update directories

--- src/LarrockComponentBlocksServiceProvider.php
+++ src/LarrockComponentBlocksServiceProvider.php
@@ -16,8 +16,8 @@ class LarrockComponentBlocksServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes.php');
-        $this->loadViewsFrom(__DIR__.'/views', 'larrock');
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadViewsFrom(__DIR__.'/../views', 'larrock');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views/vendor/larrock')
         ]);
